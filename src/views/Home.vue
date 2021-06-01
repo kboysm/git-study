67f34d9dd486c6c9770d6ac241092cb14f59795a
kboysm <lishal12@naver.com>
2021-06-01 21:13:14 +0900

    b-badge test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
   <h2>Example heading <b-badge>New</b-badge></h2>
   <h3>Example heading <b-badge>New</b-badge></h3>
   <h4>Example heading <b-badge>New</b-badge></h4>
-  <h5>Example heading <b-badge>New</b-badge></h5>
+  <h5>Example heading <b-badge>test</b-badge></h5>
   <b-form-tags noTagRemove="true" input-id="tags-limit" v-model="value" :limit="limit" remove-on-delete></b-form-tags>
 </div>
   </div>
