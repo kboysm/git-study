3b46d316c983c2d6aea40685ea33becb489113fe
kboysm <lishal12@naver.com>
2021-06-01 21:03:53 +0900

    bootstrap b-badge test

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin, BadgePlugin  } from 'bootstrap-vue'
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+Vue.use(BadgePlugin)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
-    git test-1 branch test , After diff 
+    <div>
+  <h2>Example heading <b-badge>New</b-badge></h2>
+  <h3>Example heading <b-badge>New</b-badge></h3>
+  <h4>Example heading <b-badge>New</b-badge></h4>
+  <h5>Example heading <b-badge>New</b-badge></h5>
+  <b-form-tags noTagRemove="true" input-id="tags-limit" v-model="value" :limit="limit" remove-on-delete></b-form-tags>
+</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { BBadge } from 'bootstrap-vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    BBadge
   }
 }
 </script>
