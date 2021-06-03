080c50a16a4d1ab31149b38d81394273adc12944
kboysm <lishal12@naver.com>
2021-06-03 21:44:39 +0900

    file shpage div

---
+++ src/components/test/FileShape.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div class="file-stap-1 d-flex">
+            <div class="file-stap-1-1"></div>
+            <div class="file-stap-1-2"></div>
+            <div class="file-stap-1-3"></div>
+            <div>
+                <b-badge size="lg" class="bg-dark test">삼다수</b-badge>
+            </div>
+        </div>
+        <div class="file-stap-2">
+            f림상민
+            <br>
+            ㅁㄴㅇㅁㄴㅇ<br>
+            ㅁㄴㅇㅁㄴㅇ<br>
+            ㅁㄴㅇㅁㄴㅇ<br>
+        </div>
+        <div></div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .file-stap-1 {
+        .test {
+            position:absolute;
+            left: 10px;
+            top:20px;
+        }
+        .file-stap-1-1 {
+            width:100px;
+            height: 50px;
+            background-color: yellow;
+        }
+        .file-stap-1-2 {
+            width:0;
+            height:0;
+            background-color: blue;
+            border-top: 25px solid #fff;
+            border-right: 25px solid #fff;
+            border-bottom: 25px solid yellow;
+            border-left: 25px solid yellow;
+        }
+        .file-stap-1-3 {
+            width:100px;
+            height: 50px;
+            background-color: #fff;
+        }
+    }
+    .file-stap-2 {
+        width: 300px;
+        height: 200px;
+        background-color: yellow;
+    }
+</style>
---
+++ src/components/test/RelayArrow.vue
@@ -0,0 +1,180 @@
+<template>
+    <div class="d-flex bg-dark">
+    <div class="stap-1">
+      asd
+    </div>
+    <div class="stap-2">
+      <div class="stap-2-1"></div>
+      <div class="stap-2-2"></div>
+      <div class="stap-2-3"></div>
+      <div class="stap-2-4"></div>
+    </div>
+    <div class="stap-3">
+
+    </div>
+    <div class="stap-4">
+      <span style="position:absolute; left:-10px;z-index:10;">
+      asdasdasd
+      </span>
+    </div>
+    <div class="stap-5">
+      <div class="stap-5-1"></div>
+      <div class="stap-5-2"></div>
+      <div class="stap-5-3"></div>
+      <div class="stap-5-4"></div>
+    </div>
+    <div class="stap-6"></div>
+    <div class="stap-7"></div>
+    <div class="stap-8"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.stap-1 {
+    width: 200px;
+    height: 100px;
+    position: absolute;
+    background-color: green;
+  }
+  .stap-2 {
+    .stap-2-1 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:200px;
+      border-left: 25px solid green;
+      border-right: 0 solid #fff;
+      border-top: 25px solid #fff;
+      border-bottom: 0 solid #fff;
+      z-index: 8;
+    }
+    .stap-2-2 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:200px;
+      top:25px;
+      border-left: 25px solid green;
+      border-right: 25px solid #fff;
+      border-top: 0 solid #fff;
+      border-bottom: 25px solid green;
+      z-index: 10;
+    }
+    .stap-2-3 {
+      width:50px;
+      height: 0px;
+      position:absolute;
+      left:200px;
+      top: 50px;
+      border-left: 25px solid green;
+      border-right: 25px solid #fff;
+      border-top: 25px solid green;
+      border-bottom: 0 solid #fff;
+      z-index: 10;
+    }
+    .stap-2-4 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:200px;
+      top:75px;
+      border-left: 25px solid green;
+      border-right: 0 solid #fff;
+      border-top: 0 solid #fff;
+      border-bottom: 25px solid #fff;
+      z-index: 8;
+    }
+  }
+  .stap-3 {
+    width:0;
+    height: 0;
+    position:absolute;
+    border-top: 50px solid blue;
+    border-bottom: 50px solid blue;
+    border-left: 50px solid #fff;
+    border-right: 50px solid blue;
+    left:225px;
+    z-index:9;
+  }
+  .stap-4 {
+    width: 200px;
+    height: 100px;
+    position: absolute;
+    background-color: blue;
+    left:300px;
+  }
+  .stap-5 {
+
+    .stap-5-1 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:400px;
+      border-left: 25px solid blue;
+      border-right: 0 solid #fff;
+      border-top: 25px solid #fff;
+      border-bottom: 0 solid #fff;
+      z-index: 8;
+    }
+    .stap-5-2 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:400px;
+      top:25px;
+      border-left: 25px solid blue;
+      border-right: 25px solid #fff;
+      border-top: 0 solid #fff;
+      border-bottom: 25px solid blue;
+      z-index: 10;
+    }
+    .stap-5-3 {
+      width:50px;
+      height: 0px;
+      position:absolute;
+      left:400px;
+      top: 50px;
+      border-left: 25px solid blue;
+      border-right: 25px solid #fff;
+      border-top: 25px solid blue;
+      border-bottom: 0 solid #fff;
+      z-index: 10;
+    }
+    .stap-5-4 {
+      width:50px;
+      height: 25px;
+      position:absolute;
+      left:400px;
+      top:75px;
+      border-left: 25px solid blue;
+      border-right: 0 solid #fff;
+      border-top: 0 solid #fff;
+      border-bottom: 25px solid #fff;
+      z-index: 8;
+    }
+  }
+  .stap-6 {
+    width:0;
+    height: 0;
+    position:absolute;
+    border-top: 50px solid red;
+    border-bottom: 50px solid red;
+    border-left: 50px solid #fff;
+    border-right: 50px solid red;
+    left:425px;
+    z-index:9;
+  }
+  .stap-7 {
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    background-color: red;
+    left:500px;
+  }
+  .stap-8 {
+    width: 220px;
+    height: 100px;
+    position: absolute;
+    background-color: black;
+    left: 615px;
+  }
+</style>
