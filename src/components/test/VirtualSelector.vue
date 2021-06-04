45af1cdd17f6a287bb61509e47944060ce3c95b5
kboysm <lishal12@naver.com>
2021-06-04 21:00:54 +0900

    가상 선택자로before로 의미없는 color dot 추가해보기

---
+++ src/components/test/VirtualSelector.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+            <div class="test"> 가상 선택자 before, after test</div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .test::before {
+        border-top: 1px solid blue;
+        border-right: 9px solid blue;
+        border-bottom: 1px solid blue;
+        border-left: 9px solid blue;
+        background-color: blue;
+        border-radius: 50%;
+        content: '';
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <file-shape></file-shape>
+    <!-- <file-shape></file-shape>-->
+    <virtual-selector></virtual-selector>
   </div>
 </template>
 
 <script>
 import FileShape from '@/components/test/FileShape.vue'
+import VirtualSelector from '@/components/test/VirtualSelector.vue'
 
 export default {
   name: 'Home',
   components: {
-    FileShape
+    FileShape,
+    VirtualSelector
   }
 }
 </script>
