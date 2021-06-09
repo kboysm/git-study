4ef3123f6619fbfd159d4f214f03a64af3874229
kboysm <lishal12@naver.com>
2021-06-09 20:18:12 +0900

    global css 덮어쓰기 테스트

--- src/views/TestThreePage.vue
+++ src/views/TestThreePage.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        testTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTThree
+    <div class="bg-dark" style="width:100vw;height:100vh;">
+        <div>
+            asldkjal
+        </div>
+        <div>
+        alksdjalksdjlkasd
+
+        </div>
     </div>
 </template>
+<style lang="scss">
+//       .responsive-container {
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+  
+//   }
+//   @media screen and (min-width: 1920px) {
+//     .responsive-container {
+//       justify-content: left;
+//       padding: 0 !important;
+//     }
+//   }
+  
+  
+//   .responsive-width {
+//     padding-left: 0 !important;
+//   }
+//   @media screen and (max-width: 1280px) {
+//   .responsive-width {
+//     padding-left: 0 !important;
+//   }
+//   }
+  
+//   $main-header-height: 64px;
+  
+//   .responsive-header {
+//     height: $main-header-height;
+//     padding-left: 0 !important;
+//     padding-right: 0 !important;
+//   }
+//   @media screen and (max-width: 1280px) {
+//   .responsive-header {
+//     padding-left: 0 !important;
+//     padding-right: 0 !important;
+//   }
+//   }
+  
+</style>
