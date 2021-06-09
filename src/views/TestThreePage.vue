f5a034ed18a4a3de956d3cc20500f7d5f131d374
kboysm <lishal12@naver.com>
2021-06-09 20:53:20 +0900

    scss & 연습

--- src/views/TestThreePage.vue
+++ src/views/TestThreePage.vue
@@ -1,50 +1,26 @@
 <template>
-    <div class="bg-white" style="width:100vw;height:93.8vh;">
-        <div style="width:100%; height:500px;background-color:red;">
-            asldkjal
+    <div class="test-container test2">
+        <div class="test-child">
+            <span>A</span> <span>B</span>
         </div>
-        <div>
-        alksdjalksdjlkasd
-
+        <div class="test-container-main">
+            <span>C</span><span>D</span>
+        </div>
+        <div class="test-child">
+            <span>C</span><span>D</span>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-      .responsive-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  
-  }
-  @media screen and (min-width: 1920px) {
-    .responsive-container {
-      justify-content: left;
-      padding: 0 !important;
+.test-container {
+    & > .test-child {
+        color: red;
     }
-  }
-  
-  
-//   .responsive-width {
-//     padding-left: 0 !important;
-//   }
-//   @media screen and (max-width: 1280px) {
-//   .responsive-width {
-//     padding-left: 0 !important;
-//   }
-//   }
-  
-//   $main-header-height: 64px;
-  
-//   .responsive-header {
-//     height: $main-header-height;
-//     padding-left: 0 !important;
-//     padding-right: 0 !important;
-//   }
-//   @media screen and (max-width: 1280px) {
-//   .responsive-header {
-//     padding-left: 0 !important;
-//     padding-right: 0 !important;
-//   }
-//   }
-  
+    &-main {
+        color: blue;
+    }
+    &.test2 {
+        background-color:black;
+    }
+}
 </style>
