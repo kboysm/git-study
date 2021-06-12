34cf70d6ac66323f674e0c152c5ea2e5c9462dbe
kboysm <lishal12@naver.com>
2021-06-12 23:31:12 +0900

    Gnb route path에 따른 class 적용

--- src/components/layout/TwoLayout.vue
+++ src/components/layout/TwoLayout.vue
@@ -2,7 +2,47 @@
 <div>
     <div class="bg-dark text-white" style="height:100px;">
         TwoLayout
+        <div class="d-flex justify-content-center w-100 bg-white text-dark">
+            <router-link to="/testThreePage"  :class="{ 'active': threeActive }">Three</router-link>
+             | 
+            <router-link to="/testFourPage"  :class="{ 'active': FourActive }">Four</router-link>
+        </div>
     </div>
             <router-view/>
 </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            threeActive: false,
+            FourActive: false
+        }
+    },
+    created() {
+        this.routeTestMethod()
+    },
+    beforeUpdate() {
+        this.routeTestMethod()
+    },
+    methods:  {
+        routeTestMethod( item ) {
+            this.$route.path === '/testThreePage' ? this.threeActive= true : this.threeActive= false
+            this.$route.path === '/testFourPage' ? this.FourActive= true : this.FourActive= false
+            return this.routeTest
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+    text-decoration: none;
+    outline: none;
+    color: black;
+}
+
+.active {
+        color: red;
+}
+</style>
--- src/views/TestThreePage.vue
+++ src/views/TestThreePage.vue
@@ -1,29 +1,12 @@
 <template>
     <div class="test-container test2">
-        <div :class="{ 'active': routeTestMethod }">
+        <div>
             route 테스트 진행 , route경로가 /testThreePage라면 css 효과 부여
         </div>
         
     </div>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            routeTest: false
-        }
-    },
-    computed: {
-        routeTestMethod() {
-            this.$route.path === '/testThreePage' ? this.routeTest= true : this.routeTest= false
-            return this.routeTest
-        }
-    },
-    methods: {
-        
-    }
-}
-</script>
+
 <style lang="scss" scoped>
 .test-container {
     & > .active {
