33cd4de921ec9c5ebee58b24987b9391c8589c7c
kboysm <lishal12@naver.com>
2021-07-21 20:55:28 +0900

    update:정렬 기준 2개 테스트

--- src/views/TestThreePage.vue
+++ src/views/TestThreePage.vue
@@ -1,8 +1,35 @@
 <template>
     <div class="test-container test2">
-        <div>
-            route 테스트 진행 , route경로가 /testThreePage라면 css 효과 부여
+        <div v-for="(item, index) in sortList" :key="index">
+            {{item}}
         </div>
         
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            folderList: [
+                { name: 'test' , depth: 0, sortData: '0'},
+                { name: 'test' , depth: 1, sortData: '0'},
+                { name: 'test' , depth: 0, sortData: '1'},
+                { name: 'test' , depth: 3, sortData: '0'},
+                { name: 'test' , depth: 2, sortData: '0'},
+                { name: 'test' , depth: 2, sortData: '0'},
+                { name: 'test' , depth: 1, sortData: '0'},
+            ]
+        }
+    },
+    computed: {
+        sortList() {
+            return this.folderList.sort((a,b)=> {
+                if(a.sortData < b.sortData ) return -1;
+                if(a.sortData > b.sortData ) return 1;
+                if(a.depth < b.depth ) return -1;
+                if(a.depth > b.depth ) return 1;
+            })
+        }
+    }
+}
+</script>
