8256edb321fe6c9dc1057c9bb97e16f4b71f53c7
Sanghun Han <joshephan0204@gmail.com>
2021-07-28 17:40:59 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,59 @@
 <template>
   <div id="app">
-      <!-- <side-navbar></side-navbar>
+    <!-- <side-navbar></side-navbar>
       <main-header></main-header>
       <div class="responsive-width">
         <div class="responsive-container">
         </div>
       </div> -->
-            <router-view/>
+
+    <router-view />
   </div>
 </template>
 <script>
-import SideNavbar from '@/components/SideNavbar.vue'
-import MainHeader from '@/components/MainHeader.vue'
+import SideNavbar from "@/components/SideNavbar.vue";
+import MainHeader from "@/components/MainHeader.vue";
 export default {
-  components:{
+  components: {
     SideNavbar,
-    MainHeader
-  }
-}
+    MainHeader,
+  },
+};
 </script>
 <style lang="scss">
-
+.responsive-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+@media screen and (min-width: 1920px) {
   .responsive-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-  
+    justify-content: left;
+    padding-left: 15% !important;
+    padding-right: 25% !important;
   }
-  @media screen and (min-width: 1920px) {
-    .responsive-container {
-      justify-content: left;
-      padding-left: 15% !important;
-      padding-right: 25%  !important;
-    }
-  }
-  
-  
-  .responsive-width {
-    padding-left: 240px !important;
-  }
-  @media screen and (max-width: 1280px) {
+}
+
+.responsive-width {
+  padding-left: 240px !important;
+}
+@media screen and (max-width: 1280px) {
   .responsive-width {
     padding-left: 80px !important;
   }
-  }
-  
-  $main-header-height: 64px;
-  
-  .responsive-header {
-    height: $main-header-height;
-    padding-left: 265px !important;
-    padding-right: 25px !important;
-  }
-  @media screen and (max-width: 1280px) {
+}
+
+$main-header-height: 64px;
+
+.responsive-header {
+  height: $main-header-height;
+  padding-left: 265px !important;
+  padding-right: 25px !important;
+}
+@media screen and (max-width: 1280px) {
   .responsive-header {
     padding-left: 105px !important;
     padding-right: 25px !important;
   }
-  }
-  
+}
 </style>
